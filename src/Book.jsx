dcb3508cb949fbe8f33f8dbced5013d8facbc499
Hazem Sabry <hazem.sabry@robustastudio.com>
2021-04-12 14:01:20 +0200

    fix: conditionally view authors and thumbnails

--- src/Book.jsx
+++ src/Book.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 function Book({book, onUpdateBookStatus}) {
     book.shelf = book.shelf ? book.shelf : 'none';
+    const bookThumbnailPlaceholder = require('./images/book-placeholder.png');
     return (
         <li>
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail ? book.imageLinks.thumbnail : bookThumbnailPlaceholder}")` }}></div>
                     <div className="book-shelf-changer">
                         <select value={book.shelf} onChange={(e) => onUpdateBookStatus(book, e.currentTarget.value)}>
                             <option value="move" disabled>Move to...</option>
@@ -18,9 +19,9 @@ function Book({book, onUpdateBookStatus}) {
                     </div>
                 </div>
                 <div className="book-title">{ book.title }</div>
-                <div className="book-authors">
+                { book.authors && <div className="book-authors">
                     { book.authors.map((author, idx) => <p key={idx}>{ author }</p>) }
-                </div>
+                </div>}
             </div>
         </li>
     );
--- src/SearchPage.jsx
+++ src/SearchPage.jsx
@@ -12,7 +12,7 @@ class SearchPage extends Component {
 
     state = {
         query: '',
-        books: [],
+        books: []
     }
 
     updateQuery = (query) => {
